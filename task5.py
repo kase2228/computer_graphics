175392d629edf207f084351a8f2cd281cd193f2b
kase2228 <133229334+kase2228@users.noreply.github.com>
2025-01-12 15:28:20 +0300

    readme added

--- task5.py
+++ task5.py
@@ -1,30 +1,30 @@
 import pygame
 from OpenGL.GL import *
 
-# initialize pygame and openGL
+
 pygame.init()
 glClearColor(0.5, 0.5, 0.5, 1)
 
-# create a surface object
+
 screen = pygame.display.set_mode((800,
 600))
 
-# set the background color
+
 screen.fill((255, 255, 255))
 
-# draw a triangle
+
 glBegin(GL_TRIANGLES)
-glColor3f(1, 0, 1) # set color to
+glColor3f(1, 0, 1) 
 purple
-glVertex2i(100, 100) # first vertex
-glVertex2i(200, 100) # second vertex
-glVertex2i(150, 200) # third vertex
+glVertex2i(100, 100) 
+glVertex2i(200, 100) 
+glVertex2i(150, 200)
 glEnd()
 
-# update the screen
+
 pygame.display.flip()
 
-# run the game loop
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
